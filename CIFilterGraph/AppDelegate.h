d7580d99c8941eb9906fc47c3e8aa73fc5034e54
Mete <mete.cakman@motim-technologies.com>
2013-07-03 12:27:29 +0200

    Initial Commit

---
+++ CIFilterGraph/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CIFilterGraph
+//
+//  Created by Mete Cakman on 3/07/13.
+//  Copyright (c) 2013 Mete Cakman. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ CIFilterGraph/AppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.m
+//  CIFilterGraph
+//
+//  Created by Mete Cakman on 3/07/13.
+//  Copyright (c) 2013 Mete Cakman. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
